58af3189cb8893a064f78ce845664c6308dac251
dosion9 <dosion9ood@gmail.com>
2023-10-20 10:12:10 +0900

    카드 정렬 기능

--- index.js
+++ index.js
@@ -5,35 +5,39 @@ const $searchInput = document.querySelector("#search__input");
 const $searchBtn = document.querySelector("#search__btn");
 
 api.data.topRated.then((res) => {
-  console.log(res);
+  // console.log(res);
   return createCard($topRatedArea, res);
 });
 
 const createCard = async function (parentEl, arr) {
   arr.forEach((prop, n) => {
-    const posterImg = `https://image.tmdb.org/t/p/original${prop.poster_path}`;
-
-    const tem = `
-    <div class="card" data-id="${prop.id}">
-            <div class="card-face card-front">
-                <img src="${posterImg}" alt="${prop.title} 포스터" class="card__img" />
-              <div class="card__info">
-              <h3 class="card__title">${prop.title}</h3>
-              <p>평점 : ${prop.vote_average}</p>
-              </div>
-            </div>
-            <div class="card-face card__back" style="display:none">
-              <h5 class="card__title">${prop.title}</h5>
-              <p>${prop.overview}</p>
-            </div>
-          </div>
-    `;
-
+    const tem = createCardLayout(prop);
     parentEl.insertAdjacentHTML("beforeend", tem);
     parentEl.children[n].addEventListener("click", () => {
       alert(parentEl.children[n].dataset.id);
     });
   });
+};
+
+const createCardLayout = (obj) => {
+  const imgUrl = `https://image.tmdb.org/t/p/original${obj.poster_path}`;
+  const tem = `
+    <div class="card" data-id="${obj.id}" data-rating=>
+            <div class="card__face card__face-front">
+                <img src="${imgUrl}" alt="${obj.title} 포스터" class="card__img" />
+              <div class="card__info">
+              <h3 class="card__title">${obj.title}</h3>
+              <p><b>평점</b> : ${obj.vote_average}</p>
+              </div>
+            </div>
+            <div class="card__face card__face-back" >
+              <h4 class="card__title">${obj.title}</h4>
+              <p>${obj.overview}</p>
+            </div>
+          </div>
+    `;
+
+  return tem;
 };
 
 const searchCard = function (arr, val, parentEl) {
@@ -45,6 +49,24 @@ const searchCard = function (arr, val, parentEl) {
     }
   });
 };
+
+const sortCard = function (arr, parentEl, type = "default") {
+  arr.then((data) => {
+    const byRating = [...data].sort((a, b) => b.vote_average - a.vote_average);
+    parentEl.innerHTML = null;
+
+    switch (type) {
+      case "rating":
+        createCard($topRatedArea, byRating);
+        break;
+      default:
+        createCard($topRatedArea, data);
+    }
+  });
+};
+
+// sortCard(api.data.topRated, $topRatedArea, "rating");
+// sortCard(api.data.topRated, $topRatedArea);
 
 const checkInclude = (prop, val) => {
   const condition = {
