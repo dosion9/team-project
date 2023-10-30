0cc088adf67c6385ee26ccb47532d55de0c1eeed
dosion9 <dosion9ood@gmail.com>
2023-10-30 09:44:12 +0900

    영화 검색 기능 구현

--- src/searchResult.js
+++ src/searchResult.js
@@ -2,17 +2,14 @@ import { getMovieFromKofic, getMovieFromTmdb } from "./api.js";
 
 let currentPage = 1;
 let totalMovieCount = null;
-
-// TMDB에서 가져온 영화 정보들 (제목에 검색어가 포함되어있는지 확인 후 set)
-const movieCardData = new Map();
-let printCardCount = 0;
-
-// ================================================================
-// 본 코드
-
+let fetchCheck = true;
+const movieTitleMap = new Map(); // KOFIC에서 가져온 영화 제목들 저장
+const movieCardData = new Map(); // TMDB에서 가져온 영화 정보들 (제목에 검색어가 포함되어있는지 확인 후 set)
 const queryString = new URLSearchParams(window.location.search).get("search");
 const $cardArea = document.querySelector("#movieList-search");
-const cardInstance = [];
+const $searchInput = document.querySelector("#search__input");
+
+$searchInput.value = queryString;
 
 class MovieCard {
   constructor(data) {
@@ -23,7 +20,9 @@ class MovieCard {
   }
 
   createCardLayout() {
-    const imgUrl = `https://image.tmdb.org/t/p/w500${this.poster_path}`;
+    const imgUrl = Boolean(this.poster_path)
+      ? `https://image.tmdb.org/t/p/w500${this.poster_path}`
+      : "../assets/img/noImg.jpg";
     const layout = `
                     <div class="card card-active" data-id="${this.id}">
                             <div class="card__face card__face-front">
@@ -46,16 +45,26 @@ class MovieCard {
   createCard(parentEl) {
     parentEl.insertAdjacentHTML("beforeend", this.createCardLayout());
     this.el = parentEl.children[parentEl.children.length - 1];
-    // console.log(this.el);
   }
 
   addEvent() {
     this.el.addEventListener("click", () => {
-      // 클릭 시 페이지 이동
       const detailLink = `./detail.html?id=${this.id}`;
       return (location.href = detailLink);
     });
   }
+}
+
+// 받은 array에서 카드 만드는 함수
+function createCards(arr) {
+  arr.forEach((n) => {
+    if (!movieCardData.has(n.id)) {
+      movieCardData.set(n.id);
+      const card = new MovieCard(n);
+      card.createCard($cardArea);
+      card.addEvent();
+    }
+  });
 }
 
 // 영화 필터링
@@ -64,105 +73,93 @@ function filterMovies(obj, str) {
   const titleOrigin = obj?.movieNmEn || obj?.original_title;
   const eroticGenre = obj?.repGenreNm === "성인물(에로)";
   const titleCheckKo = titleKo.replaceAll(" ", "").includes(str.replaceAll(" ", ""));
-  const titleCheckOrigin = titleOrigin.toLowerCase().replaceAll(" ", "").includes(str.toLowerCase());
+  const titleCheckOrigin = titleOrigin
+    .toLowerCase()
+    .replaceAll(" ", "")
+    .includes(str.toLowerCase().replaceAll(" ", ""));
   return !eroticGenre && (titleCheckKo || titleCheckOrigin);
 }
-
-// 영화진흥위원회 API를 사용해서 데이터 가져오기
-// 데이터 가져오기는 정상적으로 작동하지만 여기에서 받은 데이터를 가지고
-// TMDB로 요청했을 때 발생하는 문제들을 해결하지 못해 사용 X
-// async function getDataFromKofic() {
-//   const data = await getMovieFromKofic(queryString, currentPage);
-//   totalMovieCount === null ? (totalMovieCount = data.totalMovieCount) : (totalMovieCount -= 12);
-//   currentPage++; //다음번에 가져올 페이지
-//   const result = data.movieList.filter((n) => {
-//     return filterMovies(n, queryString);
-//   });
-
-//   return result;
-// }
-
-// async function getData2() {
-//   const data1 = await getDataFromKofic();
-//   data1.forEach((n, i) => {
-//     const obj = { type: "search", title: n.movieNmEn, year: n.prdtYear };
-//     getMovieFromTmdb(obj).then((res) => {
-//       // ========== 절대 나오면 안되는 경우 (시작) ==========
-//       const warning = `검색어 : ${n.movieNm}\n${res.map((i) => {
-//         return "나온거 : " + i.title + `\n`;
-//       })}이거 나오면 같은 연도 같은 한글 제목의 영화가 있다는거니 큰일났다고 생각하면된다... 절대 출력되면 안된다....`;
-//       if (res.length >= 2) {
-//         console.error(warning);
-//         alert(warning);
-//       }
-//       // ========== 절대 나오면 안되는 경우 (종료) ==========
-//       if (res.length !== 0) {
-//         snapShootTmdb.push(...res);
-//       }
-//       console.log(n.movieNm, res);
-//       // snapShootTmdb.push();
-//     });
-//   });
-
-//   fff222.forEach((n) => {
-//     const obj = { type: "search", title: n.movieNm, year: n.openDt.slice(0, 4) };
-//     getMovieFromTmdb(obj)
-//       .then((res) => {
-//         console.log(res);
-//         if (res.length !== 0) {
-//           const tmdbMovies = res.filter((i) => {
-//             return i?.title === obj.title;
-//           });
-//           tmdbMovies.length >= 2 ? console.log("TMDB에서 가져온 데이터가 2개네?") : null;
-//           console.log();
-//           return tmdbMovies;
-//         }
-//       })
-//       .then((res) => {
-//         const card = new MovieCard(res);
-//         card.createCard($cardArea);
-//         card.addEvent();
-//       });
-//   });
-// }
 
 // ================= NEW =============================
 // 검색어가 영어일 때와 한글일 때를 구분하자
 // 검색어가 한글일 때 => 바로 TMDB
 // 검색어가 영어일 때 => 영화진흥위 => TMDB
 
-// 영어일떄
-
-// 한글일떄 (완성)
-function koSearch(str) {
-  const queryTmdb = { type: "search", title: str, year: null };
-  getMovieFromTmdb(queryTmdb).then((res) => {
-    const targetMovies = res.filter((n) => {
-      return filterMovies(n, queryString);
-    });
-
-    targetMovies.forEach((n) => {
-      if (!movieCardData.has(n.id)) {
-        movieCardData.set(n.id);
-        const card = new MovieCard(n);
-        card.createCard($cardArea);
-        card.addEvent();
-      }
-    });
-  });
+function checkMoreFetch() {
+  if (totalMovieCount === null) {
+    return (fetchCheck = true);
+  }
+  if (totalMovieCount > currentPage * 12) {
+    return (fetchCheck = true);
+  } else {
+    return (fetchCheck = false);
+  }
 }
 
-koSearch(queryString);
+// 영어일떄
+async function koficSearch() {
+  const data = await getMovieFromKofic(queryString, currentPage);
+  const result = data.movieList.filter((n) => {
+    return filterMovies(n, queryString);
+  });
+
+  totalMovieCount = data.totalMovieCount;
+  currentPage++;
+
+  return result;
+}
+
+// 한글일떄 (완성)
+async function tmdbSearch(str = queryString, year = null) {
+  const queryTmdb = { type: "search", title: str, year: year };
+  const result = await getMovieFromTmdb(queryTmdb);
+  const filteredData = result.filter((n) => {
+    return filterMovies(n, queryString);
+  });
+
+  return filteredData;
+}
+
+async function movieSearch() {
+  const koreanRegex = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+  const hasKorean = koreanRegex.test(queryString);
+
+  if (hasKorean) {
+    //한글일때
+    let movieData = await tmdbSearch();
+    createCards(movieData);
+  } else {
+    //영어일때
+    const koficMovie = await koficSearch();
+    const movieTitleMap = new Map();
+    koficMovie.forEach((n) => {
+      if (n.movieNm.includes(":")) {
+        movieTitleMap.set(n.movieNm.slice(0, n.movieNm.indexOf(":")).trim());
+      } else {
+        movieTitleMap.set(n.movieNm);
+      }
+    });
+
+    for (const n of movieTitleMap) {
+      let movieData = await tmdbSearch(n);
+      createCards(movieData);
+    }
+  }
+
+  return checkMoreFetch();
+}
+
+movieSearch();
 
 // 무한 스크롤
 let timer;
 
 window.addEventListener("scroll", () => {
-  if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 400) {
+  if (fetchCheck && window.innerHeight + window.scrollY >= document.body.scrollHeight - 400) {
     if (!timer) {
       timer = setTimeout(() => {
         timer = null;
-        console.log(movieCardData);
+        movieSearch();
       }, 1000);
     }
   }
